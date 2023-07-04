2b38a8c12ab1c4af74906f89cdfc692a3faf944d
Markiyan333 <markiyan333@gmail.com>
2023-07-04 22:13:09 +0300

    add multer

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,10 +5,12 @@ const { schemas } = require('../../models/user');
 const ctrl = require('../../controlers/auth');
 
 const route = express.Router();
+
 route.get('/current', authenticate, ctrl.getCurrent);
 route.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 route.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 route.post('/logout', authenticate, ctrl.logout);
+
 route.post('/register');
 route.patch(
   '/avatars',
